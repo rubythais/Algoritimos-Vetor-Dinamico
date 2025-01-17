bcf32c662f21ad21d8300e0a25a0ea7823516de7
Tâmara Thais <t.thais@escolar.ifrn.edu.br>
2025-01-16 22:11:05 -0300

    Update Array_dinamico.hpp

--- Remove.at/Array_dinamico.hpp
+++ Remove.at/Array_dinamico.hpp
@@ -1,178 +1,156 @@
 #ifndef __ARRAY_LIST_IFRN__
 #define __ARRAY_LIST_IFRN__
-#include <stdexcept>  
 
-class arraydinamico {
-  private:
-    int *lista;
-    unsigned int size_, capacity_;
-  void increase_capacity(){
-      int *newarray = new int[capacity_+100];
-      for (int unsigned i = 0; i<size_; i++){
-        newarray[i] = lista[i];
-      }
-      delete [] lista;
-      lista = newarray;
-      capacity_= capacity_ + 100;
-  }
-  
+#include <iostream>
+#include <stdexcept>
+#include <cmath>
+
+class array_list {
+private:
+    int* data;
+    unsigned int size_;
+    unsigned int capacity_;
+
+    // Método para aumentar a capacidade do array
+    void increase_capacity() {
+        unsigned int new_capacity = capacity_ + 100; // Incremento padrão de 100
+        int* new_data = new int[new_capacity];
+        for (unsigned int i = 0; i < size_; ++i) {
+            new_data[i] = data[i];
+        }
+        delete[] data;
+        data = new_data;
+        capacity_ = new_capacity;
+    }
+
 public:
-  arraydinamico(){ 
-    size_ = 0;
-    capacity_ = 100;
-    this->lista = new int[capacity_];
-    
-      
-  }
-  ~arraydinamico(){
-    delete [] lista;
-  }
+    // Construtor
+    array_list() : data(new int[100]), size_(0), capacity_(100) {}
 
-  unsigned int size(){
-    return size_; 
-  }
-
-  unsigned int capacity(){
-    return capacity_; 
-  }
-
-  double percent_occupied() {
-    if (capacity_== 0){
-      return 0.0; 
+    // Destrutor
+    ~array_list() {
+        delete[] data;
     }
-    else {
-      return (static_cast<double>(size_) / capacity_);  
+
+    // Retorna a quantidade de elementos armazenados
+    unsigned int size() const {
+        return size_;
+    }
+
+    // Retorna a capacidade atual do array
+    unsigned int capacity() const {
+        return capacity_;
+    }
+
+    // Retorna o percentual de memória ocupada
+    double percent_occupied() const {
+        return static_cast<double>(size_) / capacity_;
+    }
+
+    // Insere elemento no índice especificado
+    bool insert_at(unsigned int index, int value) {
+        if (index > size_) return false;
+        if (size_ == capacity_) increase_capacity();
+        for (unsigned int i = size_; i > index; --i) {
+            data[i] = data[i - 1];
         }
-  }
-
-  bool insert_at(int index, int value) {
-    if (size_==capacity_){ 
-      increase_capacity();
+        data[index] = value;
+        ++size_;
+        return true;
     }
-    size_++; 
-    for (int i=size_; i > index; i--){
-      lista[i] = lista[i-1]; 
-      }
-      lista[index] = value; 
-      return true; 
-    }
-    
-  bool remove_at(int unsigned index){
-    if (index >= size_){ 
-      return false;
-    }
-    for (int unsigned i=index; i < size_; i++){
-      lista[i] = lista[i+1]; 
-    }
-    size_--; 
-    return true; 
-  }
 
-  void push_back(int value){
-    if (this->size_ == this-> capacity_){
-      increase_capacity(); 
-    }
-    this->lista[size_++] = value; 
-  }
-
-  void push_front(int unsigned value){
-    if(size_==capacity_){ 
-      increase_capacity();
-    }
-    size_++; 
-    for (int unsigned i=0; i<size_-1; i++){
-      lista[i + 1] = lista[i]; 
-      
-    }
-    lista[0] = value;
-
-  }
-
-  int get_at(int unsigned index){
-    if (index >= size_){ 
-      return -1;
-    }
-    return lista[index]; 
-   }
-
-  bool pop_back(){
-    if (size_==0){
-      return false;
-    }
-    size_--; 
-    return true;
-
-    
-  }
-  
-  bool pop_front(){
-    remove_at(0); 
-    return true;
-  }
-
-      
-      
-      
-int back(){
-  if (size_ == 0) {
-    return -1;
-  } 
-  return lista[size_-1]; 
-  }
-    
-
-
-  int front(){
-  if (size_ == 0){
-    return -1;
-  }
-  return lista[0]; 
-  } 
-
-  
-bool remove(int value){
-  int ind;
-  for (int unsigned i=0; i <= size_-1; i++){
-    if (value==lista[i]){ 
-      ind = lista[i-1]; 
-      remove_at(ind);
-    }
-  }
-    return true;
-  }  
-
-
-  int find(int value) {
-    for (unsigned int i = 0; i < size_; i++) {
-      if (lista[i] == value) {
-          return i; 
-          break;
+    // Remove elemento no índice especificado
+    bool remove_at(unsigned int index) {
+        if (index >= size_) return false;
+        for (unsigned int i = index; i < size_ - 1; ++i) {
+            data[i] = data[i + 1];
         }
+        --size_;
+        return true;
     }
-    return -1; 
-  }
 
+    // Retorna elemento no índice especificado
+    int get_at(unsigned int index) const {
+        if (index >= size_) return -1;
+        return data[index];
+    }
+
+    // Remove todos os elementos
+    void clear() {
+        size_ = 0;
+    }
+
+    // Adiciona elemento no final
+    void push_back(int value) {
+        if (size_ == capacity_) increase_capacity();
+        data[size_++] = value;
+    }
+
+    // Adiciona elemento no início
+    void push_front(int value) {
+        insert_at(0, value);
+    }
+
+    // Remove elemento do final
+    bool pop_back() {
+        if (size_ == 0) return false;
+        --size_;
+        return true;
+    }
+
+    // Remove elemento do início
+    bool pop_front() {
+        return remove_at(0);
+    }
+
+    // Retorna o elemento do final
+    int back() const {
+        if (size_ == 0) return -1;
+        return data[size_ - 1];
+    }
+
+    // Retorna o elemento do início
+    int front() const {
+        if (size_ == 0) return -1;
+        return data[0];
+    }
+
+    // Remove a primeira ocorrência de um valor
+    bool remove(int value) {
+        int index = find(value);
+        if (index == -1) return false;
+        return remove_at(index);
+    }
+
+    // Retorna o índice de um valor
+    int find(int value) const {
+        for (unsigned int i = 0; i < size_; ++i) {
+            if (data[i] == value) return i;
+        }
+        return -1;
+    }
+
+    // Retorna o número de ocorrências de um valor
+    int count(int value) const {
+        int count = 0;
+        for (unsigned int i = 0; i < size_; ++i) {
+            if (data[i] == value) ++count;
+        }
+        return count;
+    }
+
+    // Retorna a soma dos elementos
+    int sum() const {
+        int total = 0;
+        for (unsigned int i = 0; i < size_; ++i) {
+            total += data[i];
+        }
+        return total;
+    }
+};
+
+#endif // __ARRAY_LIST_IFRN__
  
-
-
-
-
-int count(int value) {
-  int qtd = 0;
-  for (int unsigned i=0; i<=size_; i++){
-    if (value==lista[i]){ 
-      qtd++; 
-    }
-  }
-   return qtd;
-  } 
-
-  int sum() {
-    int soma = 0;
-    for (int unsigned i=0; i<size_; i++) {
-      soma = soma + lista[i];  
-    }
-  return soma; 
   
 
 
